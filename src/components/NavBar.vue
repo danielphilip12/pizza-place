a961c5e7d9b861dccf603c78ac033a9fee321bc0
Daniel <daniel.gallo12@gmail.com>
2023-08-15 19:34:00 -0700

    Added NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,35 @@
 
 <template>
   <div>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+    <nav class="navbar navbar-expand-lg bg-danger">
+      <div class="container-fluid">
+        <RouterLink class="navbar-brand" to="/">Pizza Place</RouterLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <RouterLink class="nav-link bg-info me-2 rounded" to="/">Home</RouterLink>
+            <RouterLink class="nav-link bg-info me-2 rounded" to="/about">About</RouterLink>
+            <!-- <RouterLink class="nav-link" to="/">Pricing</RouterLink> -->
+          </div>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
+
+<!-- <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink> -->
