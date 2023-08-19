e4f6f3ca96aafc8e669999a3a937f532311c732f
Daniel <daniel.gallo12@gmail.com>
2023-08-18 17:42:56 -0700

    Added ingredients data to db.json and added it to the Pizza view page. Also added pizza image to PizzaView page

--- src/components/PizzaInfo.vue
+++ src/components/PizzaInfo.vue
@@ -1,18 +1,35 @@
 <template>
     <div>
-        Pizza {{ id }}
+        <h1>{{  pizza.name  }} Pizza</h1>
+        <em>Price: {{ pizza.price }}</em>
+        <br />
+        <img :src="pizza.image" class="img-fluid" />
+        <h2>Ingredients</h2>
+        <ul>
+            <li v-for="ingredient in pizza.ingredients">{{ ingredient }}</li>
+        </ul>
     </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import axios from 'axios';
 const props = defineProps({
     id: {
         required: true,
         type: Number
     }
 })
+
+const pizza = ref({});
+
+onMounted(() => {
+    axios.get(`http://localhost:3000/pizzas/${props.id}`).then(res => pizza.value = res.data)
+})
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+img {
+    max-height: 50vh;
+}
 </style>
