de95d5728d07ee3f9616182abaad05338e0e7d06
Daniel <daniel.gallo12@gmail.com>
2023-08-22 20:15:46 -0700

    Expanded Order.vue to include computed property

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,19 +1,41 @@
 <script setup>
+import { computed } from "vue";
 const props = defineProps({
   order: {
     required: true,
     type: Object,
   },
+  pizzas: {
+    required: true,
+    type: Array,
+  },
 });
+const pizzasOrdered = computed(() => {
+  const p = [];
+  for (let pOrder of props.order.pizzasOrdered) {
+    for (let pizza of props.pizzas) {
+      if (pOrder == pizza.id) {
+        p.push(pizza.name);
+      }
+    }
+  }
+  return p;
+});
+// const pizzasOrder = pizzas.filter(pizza => order.pizzasOrdered.contains(pizza.id))
 </script>
 
 <template>
-  <div class="card mb-3" >
+  <div class="card mb-3">
     <div class="card-body">
       <h5 class="card-title">{{ order.customer }}</h5>
-      <h6 class="card-subtitle mb-2 text-body-secondary">Delivery: {{ order.delivery }}</h6>
+      <h6 class="card-subtitle mb-2 text-body-secondary">
+        Delivery: {{ order.delivery }}
+      </h6>
       <p class="card-text">
         {{ order.total }}
+        <ul>
+          <li v-for="pizza in pizzasOrdered">{{ pizza }}</li>
+        </ul>
       </p>
     </div>
   </div>
--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -5,9 +5,11 @@ import { ref, onMounted } from "vue";
 
 const orders = ref([]);
 
+const pizzas = ref([])
 
 onMounted(() => {
     axios.get("http://localhost:3000/orders").then(res => orders.value = res.data);
+    axios.get("http://localhost:3000/pizzas").then(res => pizzas.value = res.data);
 })
 </script>
 
@@ -15,7 +17,7 @@ onMounted(() => {
   <div>
     <h1>Order List</h1>
     <!-- <p v-for="order in orders">{{ order.customer }} - Total: {{ order.total }}</p> -->
-    <Order v-for="order in orders" :order="order"/>
+    <Order v-for="order in orders" :order="order" :pizzas="pizzas"/>
   </div>
 </template>
 
