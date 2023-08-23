b14c2e31ccdbea39908b590fb2d3e0979902f8f7
Daniel <daniel.gallo12@gmail.com>
2023-08-22 19:58:56 -0700

    Filled out Order.vue. Used Order.vue in OrderList.vue

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,9 +1,22 @@
 <script setup>
-
+const props = defineProps({
+  order: {
+    required: true,
+    type: Object,
+  },
+});
 </script>
 
 <template>
-  <div></div>
+  <div class="card mb-3" >
+    <div class="card-body">
+      <h5 class="card-title">{{ order.customer }}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Delivery: {{ order.delivery }}</h6>
+      <p class="card-text">
+        {{ order.total }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <style lang="css" scoped></style>
--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,18 +1,21 @@
 <script setup>
 import axios from "axios";
+import Order from "./Order.vue";
 import { ref, onMounted } from "vue";
 
 const orders = ref([]);
 
+
 onMounted(() => {
-    axios.get("http://localhost:3000/orders").then(res => orders.value = res.data)
+    axios.get("http://localhost:3000/orders").then(res => orders.value = res.data);
 })
 </script>
 
 <template>
   <div>
     <h1>Order List</h1>
-    <p v-for="order in orders">{{ order.customer }} - Total: {{ order.total }}</p>
+    <!-- <p v-for="order in orders">{{ order.customer }} - Total: {{ order.total }}</p> -->
+    <Order v-for="order in orders" :order="order"/>
   </div>
 </template>
 
