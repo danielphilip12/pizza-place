d27b47de52b1882e1206872e69913cdd671435e8
Daniel <daniel.gallo12@gmail.com>
2023-08-29 19:10:04 -0700

    Add functionality to create orders. Add UUID for unique IDs

--- src/components/NewOrder.vue
+++ src/components/NewOrder.vue
@@ -1,12 +1,13 @@
 <script setup>
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 import { reactive, onMounted, ref } from "vue";
 
 const order = reactive({
   name: "",
   pizza: 0,
   delivery: false,
-  price: 0.0
+  price: 0.0,
 });
 
 const pizzas = ref([]);
@@ -21,28 +22,39 @@ const handlePizzaChange = (e) => {
   order.pizza = parseInt(e.target.value);
   try {
     order.price = pizzas.value[order.pizza - 1].price;
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     order.price = 0.0;
   }
-
-
 };
 
-const handleNameChange = (e) => {
-  order.name = e.target.value;
-};
+// const handleNameChange = (e) => {
+//   order.name = e.target.value;
+// };
 
-const handleDelivery = (e) => {
-    order.delivery = !order.delivery
-}
+// const handleDelivery = (e) => {
+//   order.delivery = !order.delivery;
+// };
 
 const submitForm = (e) => {
   e.preventDefault();
-  console.log(order.name);
-  console.log(order.pizza);
-  console.log(order.delivery);
-  console.log(order.price);
+
+  const pizzaOrder = {
+    id: uuidv4(),
+    customer: order.name,
+    pizzasOrdered: [order.pizza],
+    total: order.price,
+    delivery: order.delivery,
+  };
+  axios.post("http://localhost:3000/orders", pizzaOrder, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  order.name = "";
+  order.pizza = 0;
+  order.price = 0;
+  order.delivery = false;
 };
 </script>
 
@@ -56,7 +68,7 @@ const submitForm = (e) => {
           name="customerName"
           id="customerName"
           class="form-control"
-          @change="handleNameChange"
+          v-model="order.name"
         />
       </div>
       <div class="mb-3">
@@ -65,6 +77,7 @@ const submitForm = (e) => {
           id="pizzaList"
           class="form-select"
           @change="handlePizzaChange"
+          :value="order.pizza"
         >
           <option selected>Select a Pizza</option>
           <option v-for="pizza in pizzas" :value="pizza.id">
@@ -74,8 +87,14 @@ const submitForm = (e) => {
       </div>
       <div class="mb-3">
         <div class="form-check">
-            <input type="checkbox" name="delivery" id="delivery" class="form-check-input" @change="handleDelivery">
-            <label for="delivery" class="form-check-label">Delivery</label>
+          <input
+            type="checkbox"
+            name="delivery"
+            id="delivery"
+            class="form-check-input"
+            v-model="order.delivery"
+          />
+          <label for="delivery" class="form-check-label">Delivery</label>
         </div>
       </div>
       <p v-if="order.pizza > 0">Total: {{ pizzas[order.pizza - 1].price }}</p>
